8b76d7b109f9b571af756c5079937a8f90db4f38
AlbinaMingazova <147060006+AlbinaMingazova@users.noreply.github.com>
2023-10-14 15:55:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+let mybirthday= "30.11.1990";
+mybirthday= "мой день рождения 30.11.1990";
+console.log (mybirthday);
+
+let stroka= "stroka";
+stroka= "'добро'+'пожаловать'+'на курс'+'Альбина'";
+console.log (stroka);
+
+document.getElementById("one").innerHTML="Hello world";
+
+console.log(Infinity);
+console.log("stroka"/123);
+
+let B =1380921982180181083182199838928n;
+console.log(B);
+
+let DotChek = true;
+let DotUnchek = false;
+let test = 4<1;
+console.log(test);
+
+let nuller = null;
+console.log(nuller);
+
+
+let test1;
+console.log(test1);
+
+//alert("16.10.2023");
+
